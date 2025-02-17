a16151325e7ed6d8479f2706ad3995883bbba428
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-17 14:10:36 +0200

    Install and setup express

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-console.log('it works!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('its works');
+});
+
+app.listen(3030, () => console.log('RESTful server is running on http://localhost:3030...'));
