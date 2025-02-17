6c106984eb3c3e9195122d519bb186f91bf9d989
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-17 14:18:21 +0200

    Add mongoose and setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
+try{
+    const uri = 'mongodb://127.0.0.1:27017/furnitures';
+    await mongoose.connect(uri);
+
+    console.log('Success DB connect!');
+    
+}catch(err){
+    console.log('Connection do DB failed!');
+    console.log(err.message);
+}
+
 app.get('/', (req, res) => {
-    res.send('its works');
+    res.json({message: 'It works!'})
 });
 
 app.listen(3030, () => console.log('RESTful server is running on http://localhost:3030...'));
