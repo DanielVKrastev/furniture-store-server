2746bb1bcbd2f8fb27abaaab0313ba6aead3171d
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-17 15:58:55 +0200

    Created invalidToken model expires for 2h

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,18 +1,25 @@
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT_SECRET } from '../config.js';
+import InvalidToken from '../models/InvalidToken.js';
 
-export const auth = (req, res, next) => {
+export const auth = async (req, res, next) => {
     const token = req.headers['x-authorization'];
 
     if(! token){
         return next();
     }
 
+    // Check if token is invalidated
+    const invalidToken = await InvalidToken.findOne({token});
+    if(invalidToken){
+        return res.json({error: 'Invalid token!'});
+    }
+
     try{
         const decodedToken = jsonwebtoken.verify(token, JWT_SECRET);
         req.user = decodedToken;
     }catch(err){
-        return res.status(404).json({error: 'Invalid token!'});
+        return res.json({error: 'Invalid token!'});
     }
     
     next();
