11d72dc2746f227a10263f6e060a939245e05190
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-17 16:25:55 +0200

    Created get One functionality
    Fix Furniture model

--- server/src/service/furnitureService.js
+++ server/src/service/furnitureService.js
@@ -1,6 +1,12 @@
 import Furniture from "../models/Furniture.js";
 
 export default {
+    getAll() {
+        return Furniture.find({});
+    },
+    getOne(furnitureId){
+        return Furniture.findById(furnitureId);
+    },
     create(furnitureData, userId){
         return Furniture.create({...furnitureData, creator: userId});
     }
